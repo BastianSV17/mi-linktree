1dee4423fc56545e7e744fff2c53c179ad2781a5
Joann Vasquez Ramos <joann.vasquez@jala.university>
2025-12-24 19:14:46 -0400

    added links

--- src/script.js
+++ src/script.js
@@ -1,33 +1,33 @@
 const links = [
     {
         name: "Facebook",
-        url: "https://facebook.com/",
+        url: "https://www.facebook.com/IglesiaMayorazgoUCE",
         icon: "fa-brands fa-facebook-f",
         color: "#1877F2"
     },
     {
         name: "Instagram",
-        url: "https://instagram.com/",
+        url: "https://www.instagram.com/iglesiamayorazgo/",
         icon: "fa-brands fa-instagram",
         color: "#E4405F"
     },
     {
         name: "YouTube",
-        url: "https://youtube.com/",
+        url: "https://www.youtube.com/@iglesiamayorazgouce9420",
         icon: "fa-brands fa-youtube",
         color: "#FF0000"
     },
     {
         name: "Spotify",
-        url: "https://open.spotify.com/",
+        url: "https://open.spotify.com/show/156K4T7PQsteEFsH28y8jI?si=f5941c8f68964770",
         icon: "fa-brands fa-spotify",
         color: "#1DB954"
     },
     {
-        name: "WhatsApp",
-        url: "https://wa.me/591XXXXXXXX",
-        icon: "fa-brands fa-whatsapp",
-        color: "#25D366"
+        name: "TikTok",
+        url: "https://www.tiktok.com/@iglesia.mayorazgo",
+        icon: "fa-brands fa-tiktok",
+        color: "#000000"
     }
 ];
 
